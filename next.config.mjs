fff395dcc7f23294fbcba27d89be05f4c06f55af
Saish11568 <saishmalalikar5@gmail.com>
2026-05-12 15:59:06 +0530

    Fixed Next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/My-Portfolio',
-  images: {
-    unoptimized: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-}
+const nextConfig = {}
 
 export default nextConfig
